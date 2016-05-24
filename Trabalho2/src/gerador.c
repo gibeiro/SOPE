84867d36b9e3eb2d0d443120742f6f7dc80e86d6
root <root@localhost.localdomain>
2016-05-24 01:45:00 -0400

    Lots of changes ... long way to go.

--- Trabalho2/src/gerador.c
+++ Trabalho2/src/gerador.c
@@ -5,27 +5,44 @@
 	int t = 0;
 	while(t < atoi(argv[]))
 
-*/
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <pthread.h>
+
+pthread_mutex_t *mutex;
 
 int main(int argc, char *argv[]){
 
 	if (argc != 3) {
-			printf("Utilizacao: %s <T_GERACAO> <U_RELOGIO>\n", argv[0]);
-			return 1;
-		}
+		printf("Utilizacao: %s <T_GERACAO> <U_RELOGIO>\n", argv[0]);
+		return 1;
+	}
 
-		if (atoi(argv[1]) <= 0) {
-			printf("Parametro invalido: %s\n",argv[1]);
-			return 2;
-		}
+	if (atoi(argv[1]) <= 0) {
+		printf("Parametro invalido: %s\n",argv[1]);
+		return 2;
+	}
 
-		if(atoi(argv[2]) <= 0){
-			printf("Parametro invalido: %s\n",argv[2]);
-			return 3;
-		}
+	if(atoi(argv[2]) <= 0){
+		printf("Parametro invalido: %s\n",argv[2]);
+		return 3;
+	}
+
+	key_t key = ftok("sope",0);
+	int shmid = shmget(key,0,0);
+	mutex = (pthread_mutex_t*) shmat(shmid,0,0);
+
+	time_t end = time(NULL) + (time_t) atoi(argv[1]);
+
+	while(time(NULL) < end){
+
+	}
 
 	return 0;
 }
--- Trabalho2/src/parque.c
+++ Trabalho2/src/parque.c
@@ -1,91 +1,230 @@
 #include <pthread.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <fcntl.h>
+#include <string.h>
 
-pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+#define	N	0
+#define	S	1
+#define	E	2
+#define	O	3
 
-struct vehicle;
+typedef struct {
+
+	clock_t parking_duration;
+	int id;
+	int fifo_id;
+
+} vehicle;
+
+typedef struct {
+
+	vehicle v;
+	size_t i;
+
+} arrum_param;
+
+static const char *fifo[] = {
+		"fifoN",
+		"fifoS",
+		"fifoE",
+		"fifoO"
+};
+
+static size_t entrance[] = {N,S,E,O};
+
+static pthread_mutex_t * volatile mutex;
+static volatile size_t empty_spots;
+
+vehicle new_vehicle(int fd);
+vehicle last_vehicle();
+void vehicle_info(vehicle *v);
+
 void *ctrl_thread(void *var);
 void *arrum_thread(void *var);
 
 int main(int argc, char *argv[]){
 
+	//	Variaveis locais
+	size_t i;
+
+
+
+	//	Validar parametros
 	if (argc != 3) {
 		printf("Utilizacao: %s <N_LUGARES> <T_ABERTURA>\n", argv[0]);
-		return 1;
+		exit(EXIT_FAILURE);
 	}
-
 	if (atoi(argv[1]) <= 0) {
 		printf("Parametro invalido: %s\n",argv[1]);
-		return 2;
+		exit(EXIT_FAILURE);
 	}
-
 	if(atoi(argv[2]) <= 0){
 		printf("Parametro invalido: %s\n",argv[2]);
-		return 3;
+		exit(EXIT_FAILURE);
 	}
 
-	char fifoN[] = "fifoN";
-	char fifoS[] = "fifoS";
-	char fifoE[] = "fifoE";
-	char fifoO[] = "fifoO";
 
-	printf("Making FIFOs\n");
+
+	// Inicializar var global
+	empty_spots = atoi(argv[1]);
+
+
+
+	//	Permitir partilha de
+	//	mutexes em diferentes
+	//	processos
+	pthread_mutexattr_t mattr;
+	pthread_mutexattr_setpshared(&mattr,PTHREAD_PROCESS_SHARED);
+
+
+
+	//	Partilhar mutexes
+	pthread_mutex_t m[4];
+	size_t shm_size = 4*sizeof(m);
+
+	key_t key = ftok("parque",0);
+	int shmid = shmget(key, shm_size,IPC_CREAT | 0666);
+	if(shmid == -1){
+		perror("shmget()");
+		exit(EXIT_FAILURE);
+	}
+
+	mutex = (pthread_mutex_t*) shmat(shmid,NULL,0);
+	if(mutex == (pthread_mutex_t*)-1){
+		perror("shmat()");
+		exit(EXIT_FAILURE);
+	}
+
+	memcpy(mutex,m,shm_size);
+
+
+
+	// Inicializar mutexes
+	for(i = 0; i < 4; i++){
+		pthread_mutex_t m_tmp;
+		mutex[i] = m_tmp;
+		if(pthread_mutex_init(&mutex[i],&mattr) != 0){
+			perror("pthread_mutex_init()");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+
+
+	//	Criar FIFOs
 	mode_t mode = 0644;
-	mkfifo(fifoN, mode);
-	mkfifo(fifoS, mode);
-	mkfifo(fifoE, mode);
-	mkfifo(fifoO, mode);
+	for(i = 0; i < 4; i++)
+		if(mkfifo(fifo[i], mode) != 0){
+			perror("pthread_mutex_init()");
+			exit(EXIT_FAILURE);
+		}
 
-	printf("Creating controler threads\n");
-	pthread_t tid_N, tid_S, tid_E, tid_O;
-	pthread_create(&tid_N, NULL, ctrl_thread, fifoN);
-	pthread_create(&tid_S, NULL, ctrl_thread, fifoS);
-	pthread_create(&tid_E, NULL, ctrl_thread, fifoE);
-	pthread_create(&tid_O, NULL, ctrl_thread, fifoO);
 
-	printf("Opening FIFOs WRONLY\n");
-	int fd_N = open(fifoN, O_WRONLY);
-	int fd_E = open(fifoE, O_WRONLY);
-	int fd_S = open(fifoS, O_WRONLY);
-	int fd_O = open(fifoO, O_WRONLY);
+	//	Criar threads dos controladores
+	pthread_t tid[4];
+	for(i = 0; i < 4; i++){
+		pthread_t tid_tmp;
+		if(pthread_create(&tid_tmp, NULL, ctrl_thread, &entrance[i]) != 0){
+			perror("pthread_create()");
+			exit(EXIT_FAILURE);
+		}
+		tid[i] = tid_tmp;
+	}
 
-	printf("Waiting %s seconds ...\n",argv[2]);
+
+
+	//	Abrir FIFOs
+	int fd[4];
+	for(i = 0; i < 4; i++){
+		int fd_tmp;
+		if( (fd_tmp = open(fifo[i], O_WRONLY)) == -1){
+			perror("open()");
+			exit(EXIT_FAILURE);
+		}
+		fd[i] = fd_tmp;
+	}
+
+
+
+	//	Esperar tempo de funcionamento
+	//	do parque
 	sleep(atoi(argv[2]));
 
+
+
+	//	Fechar parque
 	vehicle close_park = last_vehicle();
 	int sizeof_vehicle = sizeof(vehicle);
-	//pthread_mutex_lock(&mutex);
 
 	printf("Done! Closing park ...\n");
-	write(fd_N,&close_park,sizeof_vehicle);
-	write(fd_S,&close_park,sizeof_vehicle);
-	write(fd_E,&close_park,sizeof_vehicle);
-	write(fd_O,&close_park,sizeof_vehicle);
+	for(i = 0; i < 4; i++){
+		if(pthread_mutex_lock(&mutex[i]) != 0){
+			perror("pthread_mutex_lock()");
+			exit(EXIT_FAILURE);
+		}
+		if(write(fd[i],&close_park,sizeof_vehicle) != 0){
+			perror("write()");
+			exit(EXIT_FAILURE);
+		}
+	}
 
+
+
+	//	Fechar FIFOs
 	printf("Done! Closing FIFOs...\n");
-	close(fd_N);
-	close(fd_S);
-	close(fd_E);
-	close(fd_O);
+	for(i = 0; i < 4; i++)
+		if(close(fd[i]) != 0){
+			perror("close()");
+			exit(EXIT_FAILURE);
+		}
 
+
+
+	//	Esperar que os controladores
+	//	terminem
 	printf("Done! Waiting for threads to close...\n");
-	pthread_join(tid_N, NULL);
-	pthread_join(tid_S, NULL);
-	pthread_join(tid_E, NULL);
-	pthread_join(tid_O, NULL);
 
-	//pthread_mutex_unlock(&mutex);
+	for(i = 0; i < 4; i++){
+
+		if(pthread_join(tid[i], NULL) != 0){
+			perror("pthread_join()");
+			exit(EXIT_FAILURE);
+		}
+
+		if(pthread_mutex_unlock(&mutex[i]) != 0){
+			perror("pthread_mutex_unlock()");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+
+
+	//	Desassociar e limpar
+	//	memoria partilhada
+	if(shmdt(mutex) != 0){
+		perror("shmdt()");
+		exit(EXIT_FAILURE);
+	}
+
+	if(shmctl(shmid, IPC_RMID, NULL) == -1){
+		perror("shmctl()");
+		exit(EXIT_FAILURE);
+	}
+
+
 
 	printf("Done! Unlinking FIFOs...\n");
-	unlink(fifoN);
-	unlink(fifoS);
-	unlink(fifoE);
-	unlink(fifoO);
+	unlink(fifo[N]);
+	unlink(fifo[S]);
+	unlink(fifo[E]);
+	unlink(fifo[O]);
 
 	printf("Done!\n");
 	return 0;
@@ -93,57 +232,138 @@ int main(int argc, char *argv[]){
 
 void *ctrl_thread(void *var){
 
-	char* fifo_name = (char*) var;
-	printf("ctrl_thread(\"%s\")\n",fifo_name);
+	arrum_param param;
 
-	int fd_read = open(fifo_name, O_RDONLY);
+	param.i = *(size_t*) var;
 
-	vehicle vehicle;
-	pthread_t arrum;
+	printf("ctrl_thread(\"%s\")\n",fifo[param.i]);
 
-	while(vehicle = new_vehicle(fd) != NULL){
 
-		if(vehicle.id == -1)
-			break;
-
-		pthread_create(&arrum, NULL, arrum_thread, vehicle);
+	//	Abrir o FIFO
+	int fd = open(fifo[param.i], O_RDONLY);
+	if(fd == -1){
+		perror("open()");
+		exit(EXIT_FAILURE);
 	}
 
-	printf("Closing ctrl_thread(\"%s\")\n",fifo_name);
+	pthread_t arrum;
+
+
+	//	Ler veiculos do FIFO
+	//	ate o parque fechar
+
+	for(;param.v.id != -1; param.v = new_vehicle(fd)){
+
+
+
+		//	Snal para fechar
+		//	o parque
+		if(param.v.id == -1){
+			printf("close veicle\n");
+			break;
+		}
+
+		if(&param.v == NULL){
+			printf("got null\n");
+			continue;
+		}
+
+		//	Read invalido
+		if(
+				param.v.id < 0 ||
+				param.v.parking_duration <= 0 ||
+				param.v.fifo_id <= 0
+		)
+			continue;
+
+		//	Criar thread arrumador
+		if(pthread_create(&arrum, NULL, arrum_thread, &param) != 0){
+			perror("pthread_create()");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	//	Fechar FIFO
+	if(close(fd) !=0){
+		perror("close()");
+		exit(EXIT_FAILURE);
+	}
+
+	printf("Closing ctrl_thread(\"%s\")\n",fifo[param.i]);
 
 	return NULL;
 
 }
 
 void *arrum_thread(void *var){
-/*
-	pthread_detach(pthread_self());
-	vehicle* vehicle = (vehicle*) var;
-	free(vehicle);
-	*/
+
 	printf("arrum_thread()\n");
+
+	//	Detach thread
+	if(pthread_detach(pthread_self()) != 0){
+		perror("pthread_detach()");
+		exit(EXIT_FAILURE);
+	}
+
+	arrum_param param = *(arrum_param*) var;
+
+	vehicle_info(&param.v);
+
+	//	Caso hajam lugares livres
+	if(empty_spots > 0){
+
+		//	Decrementar lugares livres
+		if(pthread_mutex_lock(&mutex[param.i]) != 0){
+			perror("pthread_mutex_unlock()");
+			exit(EXIT_FAILURE);
+		}
+
+		empty_spots--;
+
+		if(pthread_mutex_unlock(&mutex[param.i]) != 0){
+			perror("pthread_mutex_unlock()");
+			exit(EXIT_FAILURE);
+		}
+
+		clock_t end = clock() + param.v.parking_duration;
+
+
+		//	Esperar saida do carro
+		while(clock() < end);
+
+
+		//	Incrementar lugares livres
+		if(pthread_mutex_lock(&mutex[param.i]) != 0){
+			perror("pthread_mutex_unlock()");
+			exit(EXIT_FAILURE);
+		}
+
+		empty_spots++;
+
+		if(pthread_mutex_unlock(&mutex[param.i]) != 0){
+			perror("pthread_mutex_unlock()");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else{
+
+	}
+
+	return NULL;
 
 }
 
-typedef struct{
-
-	int parking_durantion;
-	int id;
-	int fifo_id;
-	char entrance;
-
-} vehicle;
 
 vehicle new_vehicle(int fd){
 
 	vehicle v;
 
-	read(fd,&v,sizeof(vehicle);
+	if(read(fd,&v,sizeof(vehicle)) <= 0){
+		perror("read()");
+		return *(vehicle*)NULL;
+	}
 
-	if(v.id == -1)
-		return NULL;
-	else
-		return v;
+	return v;
 }
 
 vehicle last_vehicle(){
@@ -153,4 +373,9 @@ vehicle last_vehicle(){
 	v.id = -1;
 
 	return v;
+}
+
+void vehicle_info(vehicle *v){
+
+	printf("Vehicle %d\nParking duration: %d\nFIFO id: %d\n\n",v->id,(int)v->parking_duration,v->fifo_id);
 }
